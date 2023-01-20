0a7f59bda259f8f41ccce9bd8b9f9bde6b4e7fa1
Petr Oganin <necroshizo@gmail.com>
2023-01-20 20:58:19 +0300

    1.1v

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -16,5 +16,3 @@ urlpatterns = [
     path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
 ]
-    # "refresh": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTY3NDE1ODkzOCwianRpIjoiM2M4YzhiZTViNGY5NDU1YjkyYzEwNDYxOThiNmIxZmEiLCJ1c2VyX2lkIjozfQ.P3qsjiH01FRSN5l0A7Et8PKTC15VzVfkSKlMNXCeVjs",
-    # "access": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjc0MTU4OTM4LCJqdGkiOiJiOTRlN2UxNDFhZjg0YThmOWJlMjEyNWYyYmIxNjJhYiIsInVzZXJfaWQiOjN9.UbwKkfKkLR-DftZIiamWrbkl8495SJAJ_NpuWHM8o6Q"
